bfe1b780efc6171e899c71aa1ee822ca6c27479b
rohit9414 <rohit6201164441@gmail.com>
2026-08-17 05:25:58 +0000

    Day1 update

---
+++ JavaScript/Day1.js
@@ -0,0 +1,30 @@
+const str1="Hello guis !.";
+const str2="Rohit is comming on ";
+
+// let Date=18;
+// console.log(str1.toLowerCase());
+
+let str3=`hello king codder codder`;
+// console.log(str3.indexOf("cod"));
+// console.log(str3.lastIndexOf("cod"));
+
+// console.log(str3.includes("cod"));
+
+// console.log(str3.slice(2,8));
+// console.log(str3.slice(2));
+// 
+// console.log(str3.replaceAll("cod","lad"));
+// let str="  Rohit king  ";
+// console.log(str);
+// console.log(str.trim());
+
+// const names = "Rohit Mohit Suraj Rohan Anjali";
+// console.log(names.split(","));
+
+// const n = new Date(1786944089456);
+
+// console.log(now.toLocaleString());
+
+// const now = Date.now();
+// console.log(n);
+
