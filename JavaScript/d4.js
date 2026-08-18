0779e07967c2dba75ce535db05287e09c164d938
rohit9414 <rohit6201164441@gmail.com>
2026-08-18 05:26:03 +0000

    d4 update

--- JavaScript/d4.js
+++ JavaScript/d4.js
@@ -24,3 +24,39 @@ const products = [
 //         return count+1;
 // },0)
 // console.log(total);
+
+//set implementation
+
+// const arr=[1,1,2,3,2,3,4,5,4,6,7,8,6,8];
+// const myset = new Set(arr);//S is capital
+// // console.log(myset);
+// myset.add("Rohit");
+// console.log(myset.add("Karan"));
+// console.log(myset.has("Rohit"));
+// console.log(myset.size);
+// myset.delete("Karan");
+// console.log(myset);
+// myset.clear();
+// console.log(myset);
+
+// const permissions = new Set(["read", "write", "execute"]);
+// for(let ite of permissions){
+//     console.log(ite);
+// }
+
+// concept of map
+const map1= new Map([
+  ["name", "Alice"],
+  [true, "is verified"],
+  [100, "points"]
+]
+);
+map1.set("Rohit",23);
+console.log(map1);
+console.log("-------------------");
+console.log(map1.get("Rohit"));
+console.log(map1.size);
+for( let it of map1){
+    console.log(it);
+}
+
